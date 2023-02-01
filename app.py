25d45adb402132187b2673105b1dd0856ae4e146
HorusLiang <yongqing.liang@outlook.com>
2023-02-01 18:28:38 +0100

    initial, fix bug using db.createall()

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template,request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:19990521a@localhost:5432/nameInfo'
+db=SQLAlchemy(app)
+
+class Student(db.Model):
+  __tablename__='students'
+  id=db.Column(db.Integer,primary_key=True)
+  fname=db.Column(db.String(40))
+  lname=db.Column(db.String(40))
+
+  def __init__(self,fname,lname):
+    db.create_all()
+    self.fname=fname
+    self.lname=lname
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+  fname= request.form['fname']
+  lname=request.form['lname']
+
+  student=Student(fname,lname)
+
+  db.session.add(student)
+  db.session.commit()
+
+  return render_template('success.html', data=fname)
+
+if __name__ == '__main__':
+    app.run()
